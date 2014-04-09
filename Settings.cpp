667a198b099f7feecb5d8d3965e0b98b49faf4c2
Stefano Ceccherini <stefano.ceccherini@gmail.com>
2014-04-09 21:46:50 +0200

    Fix a crash on startup when no setting file is present. Added a TODO

--- Settings.cpp
+++ Settings.cpp
@@ -58,6 +58,8 @@ Settings::Load()
 	
 	if (status == B_OK) {
 		// Copy the loaded fields to the real settings message
+		// TODO: Change this: if there is no default value,
+		// the setting is never saved!
 		BRect rect;
 		bool boolean;
 		int32 integer;
@@ -355,7 +357,7 @@ Settings::SetDefaults()
 	sSettings.AddBool(kIncludeCursor, true);
 	sSettings.AddInt32(kThreadPriority, B_NORMAL_PRIORITY);
 	sSettings.AddBool(kMinimize, false);
-	sSettings.AddString(kOutputFileFormat, "AVI");
+	sSettings.AddString(kOutputFileFormat, "");
 	
 	return B_OK;
 }
