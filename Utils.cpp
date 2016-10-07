e25f65ec7e36b9bd8ff20a7017957296cbe54c81
Stefano Ceccherini <stefano.ceccherini@gmail.com>
2016-10-07 22:06:28 +0000

    Enlarge a bit the frame when capturing a window

--- Utils.cpp
+++ Utils.cpp
@@ -105,6 +105,9 @@ UpdateMediaFormat(const int32 &width, const int32 &height,
 
 
 
+const int kFrameBorderSize = 10;
+
+
 void
 GetWindowsFrameList(BObjectList<BRect> &framesList)
 {
@@ -118,6 +121,7 @@ GetWindowsFrameList(BObjectList<BRect> &framesList)
 			client_window_info* info = get_window_info(tokenList[i]);
 			if (info != NULL && info->layer >= 3 && !info->is_mini && info->show_hide_level == 0) {
 				BRect* rect = new BRect(info->window_left, info->window_top, info->window_right, info->window_bottom);
+				rect->InsetBy(-kFrameBorderSize, -kFrameBorderSize);
 				framesList.AddItem(rect);
 				free(info);
 			}
@@ -133,6 +137,7 @@ GetWindowTokenForFrame(BRect frame)
 	int32 tokenCount = 0;
 	int32 *tokenList = NULL;
 	int32 token = -1;
+	frame.InsetBy(kFrameBorderSize, kFrameBorderSize);
 	status_t status = BPrivate::get_window_order(current_workspace(), &tokenList, &tokenCount);
 	if (status == B_OK) {
 		for (int32 i = 0; i < tokenCount && token == -1; i++) {
@@ -154,11 +159,11 @@ GetWindowTokenForFrame(BRect frame)
 BRect
 GetWindowFrameForToken(int32 token)
 {
-	int32 tokenCount = 0;
 	BRect rect;
 	client_window_info* info = get_window_info(token);
 	if (info != NULL) {
 		rect.Set(info->window_left, info->window_top, info->window_right, info->window_bottom);
+		rect.InsetBy(-kFrameBorderSize, -kFrameBorderSize);
 		free(info);
 	}
 	return rect;
