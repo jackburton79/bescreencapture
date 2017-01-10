749e596c7fff820a081d861245550d2e268c68a0
jackburton79 <stefano.ceccherini@gmail.com>
2017-01-10 07:14:08 +0000

    Added FrameRateView into OptionsWindow. It's all WIP

--- OptionsWindow.cpp
+++ OptionsWindow.cpp
@@ -3,9 +3,13 @@
  * All rights reserved. Distributed under the terms of the MIT license.
  */
  
-#include "MediaFormatView.h"
+
 #include "OptionsWindow.h"
 
+#include "FrameRateView.h"
+#include "MediaFormatView.h"
+
+#include <Box.h>
 #include <GroupLayoutBuilder.h>
 #include <GroupView.h>
 #include <LayoutBuilder.h>
@@ -17,10 +21,34 @@ OptionsWindow::OptionsWindow(Controller* controller)
 	BWindow(kWindowRect, "Encoding Settings", B_TITLED_WINDOW,
 		B_ASYNCHRONOUS_CONTROLS|B_AUTO_UPDATE_SIZE_LIMITS)
 {
+	BBox *encodingBox = new BBox("encoding options");
+	BBox* frameBox = new BBox("frame rate");
 	BLayoutBuilder::Group<>(this, B_VERTICAL)
-		.SetInsets(B_USE_DEFAULT_SPACING, B_USE_DEFAULT_SPACING)
-		.Add(new MediaFormatView(controller))
+		.SetInsets(B_USE_DEFAULT_SPACING, B_USE_DEFAULT_SPACING,
+			B_USE_DEFAULT_SPACING, B_USE_DEFAULT_SPACING)
+		.Add(encodingBox)
+		.Add(frameBox)
 		.End();
+	
+	encodingBox->SetLabel("Encoding options");
+	frameBox->SetLabel("Frame rate");
+	
+	BView* layoutView = BLayoutBuilder::Group<>()
+		.SetInsets(B_USE_DEFAULT_SPACING, B_USE_DEFAULT_SPACING,
+			B_USE_DEFAULT_SPACING, B_USE_DEFAULT_SPACING)
+		.Add(new MediaFormatView(controller))
+		.View();
+	
+	encodingBox->AddChild(layoutView);
+	
+	layoutView = BLayoutBuilder::Group<>()
+		.SetInsets(B_USE_DEFAULT_SPACING, B_USE_DEFAULT_SPACING,
+			B_USE_DEFAULT_SPACING, B_USE_DEFAULT_SPACING)
+			.Add(new FrameRateView(controller))
+		.View();
+	
+	frameBox->AddChild(layoutView);
+	
 	CenterOnScreen();
 }
 
