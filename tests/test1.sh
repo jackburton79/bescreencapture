4667655c1bdc88b8fc5ab923f5b966c66d2bf929
JackBurton79 <stefano.ceccherini@gmail.com>
2022-12-20 14:27:39 +0000

    Added test

---
+++ tests/test1.sh
@@ -0,0 +1,5 @@
+hey BeScreenCapture SET CaptureRect to "BRect(100,100,300,300)"
+hey BeScreenCapture SET Scale to "float(100)"
+hey BeScreenCapture SET RecordingTime to "uint32(5000000)"
+hey BeScreenCapture SET QuitWhenFinished to "bool(true)"
+hey BeScreenCapture DO Record
