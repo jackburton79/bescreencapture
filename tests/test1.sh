6fcadacb7069c602f50ab009b4e2748779aa5c91
JackBurton79 <stefano.ceccherini@gmail.com>
2022-12-30 09:34:09 +0000

    Updated tests

--- tests/test1.sh
+++ tests/test1.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 hey BeScreenCapture SET CaptureRect to "BRect(100,100,300,300)"
-hey BeScreenCapture SET Scale to "int32(100)"
-hey BeScreenCapture SET RecordingTime to "int32(5)"
+hey BeScreenCapture SET Scale to 100
+hey BeScreenCapture SET RecordingTime to 5
 hey BeScreenCapture SET QuitWhenFinished to "bool(true)"
 hey BeScreenCapture DO Record
--- tests/test2.sh
+++ tests/test2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 hey BeScreenCapture SET CaptureRect to "BRect(100,163,302,300)"
-hey BeScreenCapture SET Scale to "int32(75)"
-hey BeScreenCapture SET RecordingTime to "int32(2)"
+hey BeScreenCapture SET Scale to 75
+hey BeScreenCapture SET RecordingTime to 2
 hey BeScreenCapture SET QuitWhenFinished to "bool(true)"
 hey BeScreenCapture DO Record
