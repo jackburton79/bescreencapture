a22be09f057f5be5dd237afdd77f38f256e3a579
Jackburton79 <stefano.ceccherini@gmail.com>
2023-01-05 12:27:48 +0100

    UI: fix vertical spacing

--- BSCWindow.cpp
+++ BSCWindow.cpp
@@ -329,7 +329,7 @@ BSCWindow::_LayoutWindow()
 			.Add(fCamStatus)
 			.Add(fPauseButton)
 			.Add(fStartStopButton)
-			.SetInsets(B_USE_DEFAULT_SPACING)
+			.SetInsets(B_USE_DEFAULT_SPACING, 0)
 		.End();
 
 	BGroupView* outputGroup = new BGroupView(B_HORIZONTAL);
